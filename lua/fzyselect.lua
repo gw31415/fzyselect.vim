0e024c10a0a99ebe863e3d6199223f83eaa133eb
gw31415 <git@amas.dev>
2022-09-04 16:30:55 +0900

    duplicate init & add start

--- lua/fzyselect.lua
+++ lua/fzyselect.lua
@@ -1,14 +1,12 @@
 return {
-	init = function()
-		vim.ui.select = function(items, opts, on_choice)
-			return vim.fn['fzyselect#start'](
-				items, opts or {}, function(item, idx)
-				if item == vim.NIL or idx == vim.NIL then
-					on_choice()
-				else
-					on_choice(item, idx)
-				end
-			end)
-		end
+	start = function(items, opts, on_choice)
+		return vim.fn['fzyselect#start'](
+			items, opts or {}, function(item, idx)
+			if item == vim.NIL or idx == vim.NIL then
+				on_choice(nil, nil)
+			else
+				on_choice(item, idx)
+			end
+		end)
 	end
 }
