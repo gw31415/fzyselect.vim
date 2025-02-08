b350eb2e9cbdd0c5623e6238525434cd65576145
gw31415 <gw31415@amas.dev>
2025-02-08 21:10:30 +0900

    feat: export the other fzyselect#xxx functions

--- lua/fzyselect.lua
+++ lua/fzyselect.lua
@@ -1,1 +1,5 @@
-return { start = function(...) return vim.call('fzyselect#start', ...) end }
+return setmetatable({}, {
+	__index = function(_, key)
+		return function(...) return vim.call('fzyselect#' .. key, ...) end
+	end,
+})
