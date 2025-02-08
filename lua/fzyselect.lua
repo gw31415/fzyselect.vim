615b632276e10d11018d1b2bc8c4bd76935629d6
gw31415 <gw31415@amas.dev>
2025-02-08 21:05:25 +0900

    refactor: more simple code

--- lua/fzyselect.lua
+++ lua/fzyselect.lua
@@ -1,12 +1,1 @@
-return {
-	start = function(items, opts, on_choice)
-		return vim.fn['fzyselect#start'](
-			items, opts or {}, function(item, idx)
-				if item == vim.NIL or idx == vim.NIL then
-					on_choice(nil, nil)
-				else
-					on_choice(item, idx)
-				end
-			end)
-	end
-}
+return { start = function(...) return vim.call('fzyselect#start', ...) end }
